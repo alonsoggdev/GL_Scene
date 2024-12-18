12adeb75dba317919b9ecd3e00666b3116aa8b3e
alonsoggst <167032274+alonsoggdev@users.noreply.github.com>
2024-12-18 16:00:31 +0100

    Initial commit

---
+++ GL_Scene/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GL_Scene
+//
+//  Created by Alonso García on 18/12/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
